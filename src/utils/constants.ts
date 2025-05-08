0582ae221a75cfcfbdc396ef651df1f1c949abae
Diego Maldonado Zamorano <diegomz.webdev@gmail.com>
2025-05-07 21:59:59 -0400

    cambio url de api en producción

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
 export const CONSTANTS = {
-	API_URL: 'http://192.168.1.120:3000',
-	//API_URL: 'https://8e23-152-174-139-141.ngrok-free.app',
+	//API_URL: 'http://192.168.1.120:3000',
+	API_URL: 'https://8e23-152-174-139-141.ngrok-free.app',
 }
