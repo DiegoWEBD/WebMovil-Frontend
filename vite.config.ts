48b8cabf8b7d1e6ef9f7005b2d069b2bae8befa0
Diego Maldonado Zamorano <diegomz.webdev@gmail.com>
2025-05-12 21:47:16 -0400

    conversión a aplicación web progresiva (PWA)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		VitePWA({
+			manifest: {
+				icons: [
+					{
+						src: '192.png',
+						sizes: '192x192',
+						type: 'image/png',
+						purpose: 'any maskable',
+					},
+				],
+			},
+		}),
+	],
 })
