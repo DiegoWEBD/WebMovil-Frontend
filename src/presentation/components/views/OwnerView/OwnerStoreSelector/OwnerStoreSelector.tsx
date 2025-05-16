83472b91d5898df3a2294da1d1bb582c11a6ea11
Diego Maldonado Zamorano <diegomz.webdev@gmail.com>
2025-05-16 02:04:45 -0400

    corrección bug

--- src/presentation/components/views/OwnerView/OwnerStoreSelector/OwnerStoreSelector.tsx
+++ src/presentation/components/views/OwnerView/OwnerStoreSelector/OwnerStoreSelector.tsx
@@ -13,7 +13,7 @@ const OwnerStoreSelector = () => {
 	const { basicUserInfo } = useAppState()
 	const { ownerService, selectedOwnerStoreSummary, setSelectedStoreSummary } =
 		useOwnerState()
-	const [openSelector, setOpenSelector] = useState(true)
+	const [openSelector, setOpenSelector] = useState(false)
 	const selectorRef = useRef<HTMLDivElement>(null)
 
 	const { data } = useQuery<OwnerStoreSummary[] | undefined>({
